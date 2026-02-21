4e37a0e6ae174390c0a555ad923e3f41ea30feec
Abirokaya <rokayaabinash9@gmail.com>
2026-02-21 15:28:17 +0545

    config 1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Login_frontend/",
+  base: "/React_js_Practice/",
 });
